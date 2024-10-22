f0fa206a811b6025a00af64cf2b5930fa56fc349
pik <pikdev.gopi@gmail.com>
2024-10-23 00:09:32 +0200

    addBank and removeBank

---
+++ src/commands/economy/addBank.js
@@ -0,0 +1,49 @@
+const { Client, Interaction } = require('discord.js');
+const Bank = require('../../models/Bank');
+
+module.exports = {
+  name: 'addBank',
+  description: 'Add money to the bank.',
+  options: [
+    {
+      name: 'zorocoins',
+      description: 'The number of zorocoins you want to add.',
+      type: ApplicationCommandOptionType.Number,
+      required: true,
+    },
+  ],
+
+  callback: async (client, interaction) => {
+    if (!interaction.inGuild()) {
+      interaction.reply({
+        content: 'You can only run this command inside a server.',
+        ephemeral: true,
+      });
+      return;
+    }
+
+    try {
+      await interaction.deferReply();
+
+      const guildId = interaction.guild.id;
+
+      let bank = await Bank.findOne({ guildId: guildId });
+
+      if (!bank) {
+        bank = new Bank({ guildId: guildId });
+      }
+
+      const amount = interaction.options.getNumber('zorocoins');
+
+      bank.balance = bank.balance + amount;
+
+      await Promise.all([bank.save()]);
+
+      await interaction.editReply(
+        `Added **${amount}** zorocoins to the bank.`
+      );
+    } catch (error) {
+      console.log(`Error with /daily: ${error}`);
+    }
+  },
+};
