4756aaeb2aa7d37ea207ef4680a2b50ef454b585
pik <pikdev.gopi@gmail.com>
2024-10-22 22:13:10 +0200

    change the name of a file

--- src/events/ready/01registerCommands.js
+++ src/events/ready/01registerCommands.js
@@ -31,7 +31,7 @@ module.exports = async (client) => {
             options,
           });
 
-          console.log(`🔁 Edited command "${name}".`);
+          console.log(`🔁 Edited command "${name}" !`);
         }
       } else {
         if (localCommand.deleted) {
